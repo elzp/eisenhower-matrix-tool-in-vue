bedf45556dfc6b141b084f028f67ece052057511
Ela Piech <e-l-a-p@wp.pl>
2023-06-11 09:35:09 +0200

    Add FunctionalButton component

---
+++ src/components/FunctionalButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="button">{{ type }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FunctionalButton",
+  props: {
+    type: String,
+    // taskType: String,
+  },
+  data() {
+    return;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  margin: 0;
+}
+</style>
