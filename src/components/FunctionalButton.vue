b71d6d5f5a7a1dc4dc1d3e1048692681606cd2f4
elzp <piechelzbieta@wp.pl>
2023-06-26 19:47:55 +0200

    Fix: [Vue warn]: data() should return an object.

---
+++ src/components/FunctionalButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <button class="button" @click="onClick">
+      {{ type }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FunctionalButton",
+  props: {
+    type: String,
+    id: Number,
+    taskType: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onClick() {
+      if (this.type === "add new") {
+        this.$emit("settings", {
+          buttonName: "add",
+          settingsVisibility: true,
+          taskType: this.taskType,
+        });
+      } else if (this.type === "change") {
+        this.$emit("settings", {
+          buttonName: "update",
+          settingsVisibility: true,
+          taskType: this.taskType,
+          id: this.id,
+        });
+      } else if (this.type === "add") {
+        this.$emit("newTask");
+      } else if (this.type === "update") {
+        this.$emit("updateTask");
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  margin: 3px;
+}
+</style>
