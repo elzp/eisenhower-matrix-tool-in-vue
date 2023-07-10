f60f89b6ed1b3aaea1ae3ca9ba60c910b6940ab5
elzp <piechelzbieta@wp.pl>
2023-07-10 18:32:29 +0200

    Add PWA code with vue's pwa plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+const transpileDependencies = defineConfig({
   transpileDependencies: true,
 });
+module.exports = {
+  pwa: {
+    name: "Task Tool",
+    themeColor: "#ffd100"
+  },
+  ...transpileDependencies,
+}
