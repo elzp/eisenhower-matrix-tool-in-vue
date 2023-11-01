8079dc0f5f8d3f3172f570bb0d6e8469d3b62a2f
elzp <piechelzbieta@wp.pl>
2023-11-01 20:02:15 +0100

    Add styling for TaskComp

--- src/components/TaskComp.vue
+++ src/components/TaskComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="task-containter">
     <div class="name">{{ name }}</div>
     <div class="id">index: {{ id }}</div>
   </div>
@@ -14,3 +14,20 @@ export default {
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.task-containter {
+  inline-size: 150px;
+  overflow-wrap: break-word;
+  hyphens: manual;
+}
+.name {
+  text-align: left;
+  /* word-break: break-all; */
+}
+.id {
+  text-align: right;
+  font-size: 10px;
+}
+</style>
